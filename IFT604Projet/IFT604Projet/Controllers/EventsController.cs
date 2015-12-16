6475517aaaca897ddea84916174e271f61fc2ec5
Bruno-Pier Touchette <darklaus095@hotmail.com>
2015-12-16 01:35:54 -0500

    Map now supports DB tables.

---
+++ IFT604Projet/IFT604Projet/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using IFT604Projet.Models;
+using IFT604Projet.ViewModels;
+using Newtonsoft.Json;
+
+namespace IFT604Projet.Controllers
+{
+    public class EventsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var eventContext = new ApplicationDbContext();
+            
+            List<GameEvent> gameEvents = eventContext.GameEvents
+                                            .Include(evnt => evnt.Region)
+                                            .Include(evnt => evnt.Bombs)
+                                            .Where(evnt => evnt.InProgress)
+                                            .ToList();
+            return View(gameEvents);
+        }
+    }
+}
